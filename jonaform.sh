3509c438089098f46ad63c45d0cd5c65ec33886e
Jonathan Mataloni <jo.mataloni@gmail.com>
2023-01-27 16:02:49 +0100

    rm interactive install

--- jonaform.sh
+++ jonaform.sh
@@ -406,7 +406,7 @@ fi
 
 if [ $PYTHON3 -eq 1 ]; then
     echo "Installing Python..."
-    sudo apt update; sudo apt install build-essential libssl-dev zlib1g-dev \
+    sudo apt install -y build-essential libssl-dev zlib1g-dev \
     libbz2-dev libreadline-dev libsqlite3-dev curl \
     libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
     
