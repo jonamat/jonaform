1a09b32a7aabcd50684bafc66dc045ae3e512e7c
Jonathan Mataloni <jo.mataloni@gmail.com>
2023-01-27 15:58:15 +0100

    install python build deps

--- jonaform.sh
+++ jonaform.sh
@@ -406,6 +406,10 @@ fi
 
 if [ $PYTHON3 -eq 1 ]; then
     echo "Installing Python..."
+    sudo apt update; sudo apt install build-essential libssl-dev zlib1g-dev \
+    libbz2-dev libreadline-dev libsqlite3-dev curl \
+    libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
+    
     sudo apt install -y python3 python3-pip
     sudo pip3 install --upgrade pip
     curl https://pyenv.run | bash
